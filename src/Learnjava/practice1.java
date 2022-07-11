b4af17e56a8ea3742945f9555988578c82c62e2e
Raja3696 <rajrao7198@gmail.com>
2022-07-11 19:21:41 +0530

    Firstcommit

---
+++ src/Learnjava/practice1.java
@@ -0,0 +1,21 @@
+package Learnjava;
+
+public class practice1 {
+	public static void main(String[] args) {
+
+		String s ="fsdfsdf   sdf";
+		double d= 2.3534534534523453E11;
+		int i = 2147483647;
+
+		//		Scanner scan = new Scanner(System.in);
+		//		int i = scan.nextInt();
+		//		double d = scan.nextDouble();
+		//		while(scan.hasNext()){
+		//			s+=scan.next()+" ";
+		//		}
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + i);
+		//		scan.close();
+	}
+}
