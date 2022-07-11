4089260f236aa7f2ffb33519db910a8fdc55f32b
Raja3696 <rajrao7198@gmail.com>
2022-07-11 19:29:48 +0530

    Monday one commit

--- src/Learnjava/practice1.java
+++ src/Learnjava/practice1.java
@@ -16,6 +16,7 @@ public class practice1 {
 		System.out.println("String: " + s);
 		System.out.println("Double: " + d);
 		System.out.println("Int: " + i);
+		System.out.println("Monday seven thirty");
 		//		scan.close();
 	}
 }
